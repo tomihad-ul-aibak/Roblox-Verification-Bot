f7821258f24b7bf0c1bdc3490d6e38f450c1efab
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-07-06 17:59:31 +0100

    Update message.js

--- events/message.js
+++ events/message.js
@@ -10,4 +10,23 @@ module.exports = (client, message) => {
     client.commands.set(command.name, command);
   }
   
- 
+  let args = message.content.slice(config.prefix).split(' ');
+  let cmds = args.shift().toLowerCase();
+  let command = client.commands.get(cmds.toLowerCase()) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(cmds));
+  
+  if(!message.content.startsWith(config.prefix)) return;
+  if(message.author.bot) return;
+  if(message.channel.type !== 'text') return;
+  if(!command) return;
+  
+  function error(err) {
+    const errorEmbed = new MessageEmbed()
+      .setTitle('Error!')
+      .setDescription(err)
+      .setColor('RED')
+    message.channel.send(errorEmbed);
+  }
+
+  command.run(message, args, error);
+  message.delete();
+}
