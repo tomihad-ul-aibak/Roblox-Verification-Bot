7240cf9d6b31461a3f86aea40b8637e7f055192f
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-07-06 17:45:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-.
+const discord = require('discord.js');
+const client = new discord.Client();
+const noblox = require('noblox.js');
+const config = requure('./settings/config.json');
+const fs = require('fs');
+
+client.login(config.token);
+
+fs.readdir('./events/', (err, file) => {
+  if(err) return console.log(err);
+  files.forEach(file => {
+    if(!file.endsWith('.js') return;
+    const event = require('./events/${file}');
+    let eventName = file.split('.')[0];
+    
+    client.on('eventName', (null, client));
+  };
+};
