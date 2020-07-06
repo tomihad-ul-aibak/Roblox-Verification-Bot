b44dbf216248bb271f54a389faecf493c1361021
Tur-ph <67241029+Tur-ph@users.noreply.github.com>
2020-07-06 17:49:21 +0100

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,8 @@
-.
+module.exports = (client) => {
+  console.log(`${client.user.tag} is ONLINE!`);
+  console.log(`✅ | Verify.js
+✅ | Get-Roles.js
+✅ | Online.js`);
+  
+  client.user.setActivity(`${verifiedMembers} Verified Members!`, { type: 'WATCHING' });
+}
